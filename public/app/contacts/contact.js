d74acf1c6b01519e3510722e7ee81eee3a6b97de
Jason More <Jason.More@gmail.com>
2014-03-18 09:11:44 -0500

    make contact directive

---
+++ public/app/contacts/contact.js
@@ -0,0 +1,10 @@
+app.directive('contact', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/contacts/contact.html',
+    replace:true,
+    scope: {
+      contact: '=data'
+    }
+  }
+});
