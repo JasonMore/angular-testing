2c8ed80e5fb74f9e7d2d74084f2b90014a6272e9
Jason More <jasonmore@Jasons-MacBook-Pro.local>
2014-09-24 13:08:34 -0500

    updated e2e test

--- e2e/contacts.js
+++ e2e/contacts.js
@@ -1,41 +1,31 @@
-var indexPage = function () {
-  this.bottom = element(by.model('bottom'));
-
-  this.top = element(by.model('top'));
-
+function indexPage(){
   this.contacts = element.all(by.repeater('contact in contacts'));
-
   this.firstContact = element(by.repeater('contact in contacts').row(0));
-
-  this.navigate = function () {
-    browser.get('http://localhost:3333');
-  };
-};
-
+  browser.get('http://localhost:3333');
+}
 
 describe('open main page >', function () {
   var index = new indexPage();
-  index.navigate();
 
   it('shows 5 contacts', function () {
     expect(index.contacts.count()).toEqual(5);
   });
 
   describe('contact >', function () {
-    it('it goes up', function () {
-      var age = index.firstContact.findElement(by.binding('{{contact.age}}'));
-      var picture = index.firstContact.findElement(by.tagName('a'));
+    it('clicking it goes up in age', function () {
+      var age = index.firstContact.element(by.binding('{{contact.age}}'));
+      var picture = index.firstContact.element(by.css('a[ng-click="addYear()"]'));
 
       var originalAge;
-      age.getText().then(function (originalAgeText) {
-        originalAge = parseInt(originalAgeText);
+      age.getText().then(function (ageText) {
+        originalAge = parseInt(ageText);
         picture.click();
 
         age.getText().then(function (newAgeText) {
           var newAge = parseInt(newAgeText);
           expect(newAge).toEqual(originalAge + 1);
-        });
-      });
+        })
+      })
     });
   });
 });
